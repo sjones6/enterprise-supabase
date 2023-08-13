84cb2d378beda21d9376bf86b2ecf52de1b3c044
Spencer Jones <jones.spencera@gmail.com>
2023-08-13 00:06:12 -0500

    groups

--- packages/enterprise-supabase-react/src/components/pages/GroupsPage.tsx
+++ packages/enterprise-supabase-react/src/components/pages/GroupsPage.tsx
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-=======
 import { Group } from "enterprise-supabase";
->>>>>>> 450d9dd (progress)
 import { DialogCreateGroup } from "../dialogs/DialogCreateGroup";
 import { GroupsTable } from "../tables/GroupsTable";
 import { Button } from "../ui/button";
 
-<<<<<<< HEAD
-export const GroupsPage = (): JSX.Element => {
-=======
 type GroupsPageProps = {
   onCreateGroup: (group: Group) => void;
   onEditGroup: (group: Group) => void;
@@ -20,17 +14,10 @@ export const GroupsPage = ({
   onDeleteGroup,
   onEditGroup,
 }: GroupsPageProps): JSX.Element => {
->>>>>>> 450d9dd (progress)
   return (
     <div className="h-full flex flex-col">
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-2xl">Groups</h1>
-<<<<<<< HEAD
-        <DialogCreateGroup trigger={<Button>Create group</Button>} />
-      </div>
-      <div className="flex-grow overflow-scroll">
-        <GroupsTable />
-=======
         <DialogCreateGroup
           form={{
             onSuccess: onCreateGroup,
@@ -40,7 +27,6 @@ export const GroupsPage = ({
       </div>
       <div className="flex-grow overflow-scroll">
         <GroupsTable onEditGroup={onEditGroup} onDeleteGroup={onDeleteGroup} />
->>>>>>> 450d9dd (progress)
       </div>
     </div>
   );
